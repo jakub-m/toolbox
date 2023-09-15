7004a66a23e29360130454bb07069725b414fdb0
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2023-09-15 11:56:01 +0200

    wal berbose

--- cli/wall-of-text/main.go
+++ cli/wall-of-text/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
+	flagVerbose := false
+	flag.BoolVar(&flagVerbose, "v", false, "verbose")
+	flag.Parse()
+	if flagVerbose {
+		log.SetOutput(os.Stderr)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
