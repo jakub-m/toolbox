f1843f47633ede0c81413d418812bceab6c91db7
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2023-12-26 22:18:36 +0100

    test

--- tscalc/cli/main_test.go
+++ tscalc/cli/main_test.go
@@ -25,15 +25,15 @@ func TestCalc(t *testing.T) {
 		{"  100", "1970-01-01T00:01:40+00:00"},
 		{"1970-01-01T00:01:40+00:00", "100.000000"},
 		{"1m + 1s", "1m1s"},
-		// {"1m+1s", "1m1s"}, // TODO
+		// {"1m+1s", "1m1s"}, // TODO handle spaces
 		{"1970-01-01T00:00:00+00:00 + 1m40s", "1970-01-01T00:01:40+00:00"},
 		{"1970-01-01T00:01:40+00:00 - 1970-01-01T00:00:00+00:00", "1m40s"},
 		{"200 - 100", "1m40s"},
 		{"NOW - 1h", "1969-12-31T23:00:00+00:00"},
 		{"NOW - NOW", "0s"},
-		// {"NOW - NOW + 1h", "1h0m0s"}, // TODO
+		//{"NOW - NOW + 1h", "1h0m0s"}, // TODO shoudl be ((now - now):period-expr + 1h):period-expr, not (now - (now + 1h):date-expr):period-expr
 		{"1s + 1s - 1s", "1s"},
-		// {"1s - 1s + 1s", "1s"}, // TODO
+		// {"1s - 1s + 1s", "1s"}, // TODO recognise "sub period" as "add minus period"?
 	} {
 		t.Run(fmt.Sprintf("%s == %s", tc.input, tc.expected), func(t *testing.T) {
 			actual, err := handleLine(tc.input)
